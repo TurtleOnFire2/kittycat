862337f6671e89ee2f9cce7d3f22b47a57425823
TurtleOnFire2 <ijon.lorenz@gmail.com>
2026-08-21 00:29:40 +0200

    cancel placing conduit

--- src/client/java/kitty/cat/mixin/client/MultiPlayerGameModeMixin.java
+++ src/client/java/kitty/cat/mixin/client/MultiPlayerGameModeMixin.java
@@ -2,13 +2,16 @@ package kitty.cat.mixin.client;
 
 import kitty.cat.features.dungeons.Storm;
 import kitty.cat.features.kuudra.AutoGFS;
+import kitty.cat.features.kuudra.Fixes;
 import kitty.cat.features.kuudra.Stun;
 import kitty.cat.utils.BoneUtils;
 import kitty.cat.features.kuudra.RendMacro;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,5 +26,10 @@ public class MultiPlayerGameModeMixin {
         RendMacro.INSTANCE.useItem(player, interactionHand, cir.getReturnValue());
         Stun.INSTANCE.useItem(player, interactionHand, cir.getReturnValue());
         AutoGFS.INSTANCE.useItem(player, interactionHand, cir.getReturnValue());
+    }
+
+    @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
+    void onUseItem(LocalPlayer player, InteractionHand hand, BlockHitResult blockHit, CallbackInfoReturnable<InteractionResult> cir) {
+        if (Fixes.INSTANCE.cancelPlacement(player)) cir.cancel();
     }
 }
--- src/client/kotlin/kitty/cat/features/kuudra/Fixes.kt
+++ src/client/kotlin/kitty/cat/features/kuudra/Fixes.kt
@@ -4,15 +4,25 @@ import kitty.cat.KittycatClient.mc
 import kitty.cat.features.Feature
 import kitty.cat.gui.categories.Categories
 import kitty.cat.utils.uuid
+import net.minecraft.world.entity.player.Player
 
 object Fixes : Feature("Fixes", "", Categories.Category.KUUDRA){
     val hollowFix = booleanSetting("Hollow wand fix", false)
+    val cancelPlacingConduit = booleanSetting("Cancel placing conduit", false)
+
+    fun cancelPlacement(player: Player): Boolean {
+        val uuid = player.mainHandItem.uuid()
+
+        if (uuid != "ETHERWARP_CONDUIT") return false
+
+        return (enabled && cancelPlacingConduit.value)
+    }
 
     fun clickThrough(): Boolean {
         val uuid = mc.player?.mainHandItem?.uuid()
 
         if (uuid != "HOLLOW_WAND") return false
 
-        return !(!enabled || !hollowFix.value)
+        return (enabled && hollowFix.value)
     }
 }
