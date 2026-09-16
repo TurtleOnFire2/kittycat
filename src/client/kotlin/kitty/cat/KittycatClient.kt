58cb558c21bfd1af9c989b03fe6206d1b69916b0
TurtleOnFire2 <ijon.lorenz@gmail.com>
2026-09-16 21:07:11 +0200

    Restore Bestiary ESP and its configuration screen

--- src/client/kotlin/kitty/cat/KittycatClient.kt
+++ src/client/kotlin/kitty/cat/KittycatClient.kt
@@ -14,6 +14,7 @@ import kitty.cat.features.kuudra.RendMacro
 import kitty.cat.features.misc.ChatMacros
 import kitty.cat.features.misc.Pests
 import kitty.cat.features.debug.PearlLandingDebug
+import kitty.cat.features.visual.BestiaryESP
 import kitty.cat.features.visual.CatEars
 import kitty.cat.features.visual.ClickGui as ClickGuiFeature
 import kitty.cat.gui.Hud
@@ -143,6 +144,12 @@ object KittycatClient : ClientModInitializer {
 			dispatcher.register(
 				literal("kc")
 					.then(
+						literal("be").executes {
+							BestiaryESP.openGui = true
+							1
+						}
+					)
+					.then(
 						literal("gui").executes { context ->
 							openGui = true
 							1
@@ -196,6 +203,7 @@ object KittycatClient : ClientModInitializer {
 		Pests.register()
 		Hud.register()
 		BestiaryHud.register()
+		BestiaryESP.register()
 		ChatMacros.register()
 		Schedule.register()
 		Storm.register()
